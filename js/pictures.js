65a62bd70ddc9bba8d2939781f6d822c135914eb
Irina Zhuravleva <sparrow113@rambler.ru>
2018-06-15 17:22:54 +0300

    Появились клоны одного фото

--- js/pictures.js
+++ js/pictures.js
@@ -11,12 +11,65 @@ function getRandomElementFromArray(array) {
 }
 
 
-var pictures = [
-  {
-    url: '../photos' + getRandomNumber(1, 25) + '.jpg',
-    likes: getRandomNumber(15, 200),
-    comments: getRandomElementFromArray(COMMENTS),
-    description: getRandomElementFromArray(DESCRIPTION)
-  },
+//создаю массив, состоящий из 25 фото
 
-];
+var infoPictures = [];
+for (var i = 0; i < 25; i++) {
+  var picture = {
+      url: '../photos' + getRandomNumber(1, 25) + '.jpg',
+      likes: getRandomNumber(15, 200),
+      comments: getRandomElementFromArray(COMMENTS),
+      description: getRandomElementFromArray(DESCRIPTION)
+  };
+
+  infoPictures.push(picture);
+};
+
+
+//на его основе создаю дом-элементы для фото и заполняю их данными из массива
+//вот это вообще не понимаю, что такое
+var renderPicture = function (superPicture, i) {
+  var template = document.querySelector('#picture');
+  var element = template.content.cloneNode(true);
+  element.querySelector('.picture__link').innerHTML = picture.url;
+  element.querySelector('.picture__stat--likes').textContent = picture.likes;
+  element.querySelector('.picture__stat--comments').textContent = picture.comments;
+};
+
+
+
+// отрисовываю все элементы в блок .pictures при помощи DocumentFragment
+
+var listPictures = document.querySelector('.pictures');
+//var fragmentPicture = document.createDocumentFragment();
+var pictureTemplate = document.querySelector('.picture');
+
+
+for (var n = 0; n < 25; n++) {
+
+  var pictureElement = pictureTemplate.cloneNode(true);
+
+  // pinElement.className = 'pin';
+  // pinElement.classList.add('pin--' + n );
+
+  // pinElement.style.left = infoBookings[n].location.x + 'px';
+  // pinElement.style.top = infoBookings[n].location.y + 'px';
+  // pinElement.innerHTML = '<img src="' + infoBookings[n].author.avatar + '" class="rounded" width="40" height="40">';
+   //fragmentPicture.appendChild(pictureElement);
+listPictures.appendChild(pictureElement);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
